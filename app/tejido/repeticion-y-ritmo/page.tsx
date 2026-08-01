1b980332e0537fd5c18ba156647403876ef91085
b8vpk8465h-creator <b8vpk8465h-creator@users.noreply.github.com>
2026-08-01 19:09:40 +0900

    Remove initial textile placeholder entries

--- app/tejido/repeticion-y-ritmo/page.tsx
+++
@@ -1,7 +0,0 @@
-import { EntryPage } from "../../_components/editorial";
-import { getCollection, getEntry } from "../../../content/collections";
-
-export default function RepetitionAndRhythmPage() {
-  const collection = getCollection("tejido")!;
-  return <EntryPage collection={collection} entry={getEntry("tejido", "/tejido/repeticion-y-ritmo")!} />;
-}
